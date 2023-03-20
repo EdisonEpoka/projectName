682322976e163619097e52a19b23c8fcf7239c76
EdisonEpoka <edison.reshketa@gmail.com>
2023-03-20 15:09:51 +0100

    Create Classroom.py

---
+++ Classroom.py
@@ -0,0 +1,7 @@
+class Classroom:
+  def __init__(self, capacity, pcNumber, projector, buldingNr, classroomName):
+    self.capacity = capacity
+    self.pcNumber = pcNumber
+    self.projector = projector
+    self.buldingNr = buldingNr
+    self.classroomName = classroomName
